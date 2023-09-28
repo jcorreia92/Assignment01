02bd9ef0e386b42b3dc804e891ae28ed53307a22
Jake Correia <jacobrcorreia@gmail.com>
2023-09-27 18:52:53 -0700

    completed assignment

---
+++ js/script.js
@@ -0,0 +1,123 @@
+// STEP 1
+// someMonth
+// theMonth
+// currentMonth
+// summerMonth
+// myLibraryFunction
+
+// STEP 2
+// 5.7          // numeric literal
+// "literal"    // string literal
+// false        // Boolean literal
+// null         // null literal
+
+// STEP 3
+// let thisExp = 7 * 8 * 9
+// let thatExp = console.log('You have a virus!')
+
+// STEP 4
+// let firstName, lastName, address, city, state, zipCode, yourAge, refSource, mayContact                   // Camel Casing
+// let strFirstName, strLastName, strAddress, strCity, intZipCode, intYourAge, strRefSource, blnMayContact  // Hungarian Notation
+
+// STEP 5
+
+    // Variation I
+// let firstName
+// let zipCode
+// let mayContact
+
+// firstName = 'Jake'
+// zipCode = 92123
+// mayContact = true
+
+    // Variation II
+// let firstName = 'Jake'
+// let zipCode = 92123
+// let mayContact = true
+
+    // Variation III
+// let firstName = 'Jake', zipCode = 92123, mayContact = true
+
+// STEP 6
+// let coercedNum = 5 + '5'
+// console.log(coercedNum) // result = 55
+
+// STEP 7
+// let coercedToStr = true + ' false'
+// let coercedToNum = true + 5
+// console.log(coercedToStr) // prints 'true false'
+// console.log(coercedToNum) // prints 6 (true = 1)
+
+// STEP 8
+
+// The given string literal is not valid since several of the characters need to be "escaped"
+// This can be solved by using back ticks instead of single or double quotes
+// let someString = `Who once said, "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former."`
+// console.log(someString)
+
+// STEP 9
+// let nullVar = null
+// let undefVar = NaN
+// console.log(nullVar)
+// console.log(undefVar)
+
+// STEP 10
+// console.log(typeof('string'))
+// console.log(typeof(5))
+// console.log(typeof(true))
+// console.log(typeof(console.log))
+// console.log(typeof(NaN))
+
+// STEP 11
+// alert('Hello ' + 'Jake Correia' + ', welcome to the JavaScript class!')
+
+// STEP 12
+// let name = 'Jake Correia'
+// alert('Hello ' + name + ', welcome to the JavaScript class!')
+
+// STEP 13
+// let name = 'Jake Correia'
+// let course = 'JavaScript'
+// alert(`Hello ${name}, welcome to the ${course} class!`)
+
+// STEP 14
+// let name = 'Jake Correia'
+// let course = 'JavaScript'
+// alert(`Hello ${name}.\nWelcome to the ${course} class!`)
+
+// STEP 15
+// let userName = prompt('Please enter your name:')
+// let course = 'JavaScript'
+// alert(`Hello ${userName}.\nWelcome to the ${course} class!`)
+
+// STEP 16
+// let userName = prompt('Please enter your name:')
+// let userCourse = prompt('Please enter the name of your course:')
+// alert(`Hello ${userName}.\nWelcome to the ${userCourse} class!`)
+
+// STEP 17
+// let x = 10, y = 20
+// console.log(x + y)
+
+// STEP 18
+// let x = 20
+// x += 20
+// console.log(x)
+
+// STEP 19
+// let x = 20
+// x *= 5
+// console.log(x)
+
+// STEP 20
+// let x = 20 % 3
+// x /= 1
+// console.log(x)
+
+// STEP 21
+// let x = 5, y = 3
+// console.log(x == 5 || y == 4)
+
+// STEP 22
+// let x = 5, y = 3
+// console.log(x > y && y <= 2)
